cd17c4f44ed8f42da12b2bc2387bd75492a7a2bd
theJuniorProgrammer3 <175095475+theJuniorProgrammer3@users.noreply.github.com>
2025-07-26 20:33:59 +0700

    Prototipe #2

--- main.cpp
+++ main.cpp
@@ -6,27 +6,62 @@ int main() {
 	initscr();
 	keypad(stdscr, TRUE);
 	nodelay(stdscr, TRUE);
-	std::vector<bool> cond = {0, 0, 0, 1, 0, 0, 0};
+	std::vector<char> cond = {'0', 'b', '0', 'c', '0', '0', '0', '0', 'b', 'b', 'b', '0'};
 	/*printw("Click 'c' to skip cutscene");
 	napms(4000);*/
+	int bCount = 0;
 	int inp;
+	noecho();
 	while(true) {
 		clear();
 		inp = getch();
 		if(inp == KEY_RIGHT) {
-			auto it = std::find(cond.begin(), cond.end(), 1);
+			auto it = std::find(cond.begin(), cond.end(), 'c');
 			int index = it - cond.begin();
-			cond[index] = 0;
-			cond[index + 1] = 1;
+			if(cond[index + 1] == '0') {
+			cond[index] = '0';
+			cond[index + 1] = 'c';
+			}
 		} else if(inp == KEY_LEFT) {
-			auto it = std::find(cond.begin(), cond.end(), 1);
+			auto it = std::find(cond.begin(), cond.end(), 'c');
 			int index = it - cond.begin();
-			cond[index] = 0;
-			cond[index - 1] = 1;
+			if(cond[index - 1] == '0') {
+			cond[index] = '0';
+			cond[index - 1] = 'c';
+			}
+		} else if(inp == 'b') {
+			auto it = std::find(cond.begin(), cond.end(), 'c');
+			int index = it - cond.begin();
+			if(cond[index + 1] == 'b') {
+			cond[index + 1] = '0';
+			bCount++;
+			}
+		} else if(inp == 'v') {
+			auto it = std::find(cond.begin(), cond.end(), 'c');
+			int index = it - cond.begin();
+			if(cond[index - 1] == 'b') {
+			cond[index - 1] = '0';
+			bCount++;
+			}
+		} else if(inp == 'p') {
+			auto it = std::find(cond.begin(), cond.end(), 'c');
+			int index = it - cond.begin();
+			if(bCount != 0) {
+				cond[index + 1] = 'b';
+				bCount--;
+			}
+		} else if(inp == 'o') {
+			auto it = std::find(cond.begin(), cond.end(), 'c');
+			int index = it - cond.begin();
+			if(bCount != 0) {
+				cond[index - 1] = 'b';
+				bCount--;
+			}
 		} else if(inp == 'q') break;
 		for(auto m : cond) {
-			printw((m == 1 ? "[@]" : "[ ]"));
+			printw((m == 'c' ? "[@]" : (m == 'b' ? "[X]" : "[ ]")));
 		}
+		printw("\nBlock in inventory: %d", bCount);
 		refresh();
 		napms(100);
 	}
